13792d9317440d2b1cbf5f0e4dc5bb619e3bb340
Sheheryar Parvaz <skipper308@hotmail.ca>
2019-04-21 10:51:42 -0400

    Clean up redundant wiring in RegFile

--- src/regfile.v
+++ src/regfile.v
@@ -6,21 +6,12 @@ module RegFile
   , output [XLEN-1:0] rs1_out, rs2_out
  );
 
-    reg  [XLEN-1:0] reg_file [31:1];
-    wire [XLEN-1:0] reg_data [31:0];
+    reg [XLEN-1:0] reg_file [31:1];
 
-    assign reg_data[0] = {XLEN{1'b0}};
+    assign rs1_out = rs1 != 0 ? reg_file[rs1] : 0;
+    assign rs2_out = rs2 != 0 ? reg_file[rs2] : 0;
 
-    generate
-        genvar i;
-        for (i = 1; i < 32; i = i+1)
-            assign reg_data[i] = reg_file[i];
-    endgenerate
-
-    assign rs1_out = reg_data[rs1];
-    assign rs2_out = reg_data[rs2];
-
-    // Assign to reg_file iff rd_w and rd != 0
+    // Assign to reg_file iff rd_w == 0 and rd != 0
     always @(posedge clk) begin
         if (rd_w & |rd)
             reg_file[rd] <= rd_in;
